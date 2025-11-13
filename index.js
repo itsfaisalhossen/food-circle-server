48ad7987dd558571bb71e79ff39bae46a7fb3f86
itsfaisalhossen <itsfaisalhossen@gmail.com>
2025-11-13 09:04:27 +0200

    Addded dotenv file to dynamic asscess to pass user

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 // middileware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://foodCirlceDb:mjTrgEOU7rRn0jGc@cluster0.uvhdimh.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvhdimh.mongodb.net/?appName=Cluster0`;
+console.log(process.env.DB_USER);
 
 const client = new MongoClient(uri, {
   serverApi: {
